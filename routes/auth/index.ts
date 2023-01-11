7f364f593d7b15b71e162d5b3f3374946c761a42
SyedTayyabUlMazhzar <stayyabmazhar19991@gmail.com>
2023-01-11 19:41:08 +0500

    Insert user information to db on signup request.

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -2,6 +2,7 @@ import express, { Request } from "express";
 import { getDb } from "../../db/conn.js";
 import { SignUpBody } from "../types.js";
 import { RouteUrl } from "../constants.js";
+import { v4 as uuidv4 } from "uuid";
 
 const authRoutes = express.Router();
 export default authRoutes;
@@ -10,6 +11,9 @@ authRoutes
   .route(RouteUrl.SignUp)
   .post(async function (req: Request<{}, {}, SignUpBody>, res) {
     const db = getDb();
+    const body = req.body;
 
+    const user = { ...body, userId: uuidv4() };
+    await db.collection("users").insertOne(user);
     res.send("Signup successs");
   });
