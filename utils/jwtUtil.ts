e13a34e7597797bb95b4b14d1ef31664daeb171c
SyedTayyabUlMazhzar <stayyabmazhar19991@gmail.com>
2023-01-24 19:41:55 +0500

    Jwt: Create verifyToken method.

--- utils/jwtUtil.ts
+++ utils/jwtUtil.ts
@@ -10,8 +10,27 @@ function generateAccessToken(user: Partial<User>) {
   return jwt.sign(user, JWT_SECRET, { expiresIn: "18s" });
 }
 
+type VerifyToken =
+  | { error: jwt.VerifyErrors }
+  | { error: null; user: Pick<User, "userId"> };
+
+function verifyToken(token: string) {
+  return new Promise<VerifyToken>((resolve) => {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+      if (err) {
+        resolve({ error: err });
+      } else {
+        const { iss, sub, aud, exp, nbf, iat, jti, ..._user } =
+          user as jwt.JwtPayload;
+        resolve({ error: null, user: _user as Pick<User, "userId"> });
+      }
+    });
+  });
+}
+
 const Jwt = {
   generate: generateAccessToken,
+  verify: verifyToken,
 };
 
 export default Jwt;
