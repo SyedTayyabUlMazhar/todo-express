11f602095c5ba3c67901a39aa303f5b2098f4ff6
SyedTayyabUlMazhzar <stayyabmazhar19991@gmail.com>
2023-01-26 13:59:20 +0500

    Add create post route skeleton.

--- routes/constants.ts
+++ routes/constants.ts
@@ -6,6 +6,7 @@ export const RouteUrl = {
   SignIn: "/signin",
 
   Post: "/post",
+  createPost: "/create",
 };
 
 export const RouteSchemaMap = {
--- routes/post/index.ts
+++ routes/post/index.ts
@@ -1,4 +1,13 @@
 import express from "express";
+import ApiResponse from "../../utils/responseUtil.js";
+import { RouteUrl } from "../constants.js";
+import { AuthorizedRequest } from "../types";
 
 const postRoutes = express.Router();
 export default postRoutes;
+
+postRoutes.route(RouteUrl.createPost).post((req: AuthorizedRequest, res) => {
+  if (!req.user) return;
+  
+  res.json(ApiResponse.success(req.user));
+});
